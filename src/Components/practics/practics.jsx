1b4189122603b96a4191c267513cb32b6c1b202f
shaadi1478 <sheikhshaadi137@gmail.com>
2025-10-13 23:05:50 +0600

    some component add

---
+++ src/Components/practics/practics.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+
+const testimonials = [
+    {
+        text: `“I requested a UI update gig and received excellent work from Maria! Talented young UI/UX designer who exceeded expectations! Great experience and quick feedback. Will definitely work with again!”`,
+        name: "Maria Johnson",
+        designation: "UI/UX Designer",
+        img: "https://i.postimg.cc/XqPDjT5w/testim1.png",
+    },
+    {
+        text: `“Best Seller for website design, she goes above and beyond to please her clients. She is by my side the entire time. I will keep using her for additional projects. Thank You so much Techoweb.”`,
+        name: "John Smith",
+        designation: "Project Manager",
+        img: "https://i.postimg.cc/X7ysK1FL/testim2.png",
+    },
+    {
+        text: `“I requested a UI update gig and received excellent work from Maria! Talented young UI/UX designer who exceeded expectations! Great experience and quick feedback. Will definitely work with again!”`,
+        name: "Sara Lee",
+        designation: "Marketing Head",
+        img: "https://i.postimg.cc/Kc16VNBr/testim3.png",
+    },
+    {
+        text: `“Nurjahan96 is amazing. The website exceeded my expectations. She made all requested changes and ensured everything was exactly how I wanted it. Thank you so much!”`,
+        name: "Alex Brown",
+        designation: "Product Manager",
+        img: "https://i.postimg.cc/g0M7tXJd/testim4.png",
+    },
+    {
+        text: `“I would recommend nurjahan96 to everyone who wants to work with a professional. Amazing job and communication within the timeframe. One of the best designers you can find!”`,
+        name: "Lily Adams",
+        designation: "CEO",
+        img: "https://i.postimg.cc/LssCP9VN/testm5.png",
+    },
+    {
+        text: `“A master of UI/UX! This designer crafts digital experiences that captivate. With a keen eye for detail and user-centric approach, they deliver top-notch results. Highly impressed!”`,
+        name: "Rosy Flower Gaggero",
+        designation: "CEO",
+        img: "https://i.postimg.cc/bJtR0sSq/testim6.png",
+    },
+];
+
+const practics = () => {
+    return (
+        <div>
+            {testimonials.map((item, index) =>(
+                <div>
+
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default practics;
